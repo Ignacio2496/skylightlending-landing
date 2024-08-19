708e92fb925a14587c9c64d451de0708f755f501
Alejandro98123 <176923232+Alejandro98123@users.noreply.github.com>
2024-08-19 10:35:55 -0300

    Agregar Nav

---
+++ src/components/AcmeLogo.tsx
@@ -0,0 +1,6 @@
+import imgAcme from "../assets/logo-f-w.png"
+
+export const AcmeLogo = () => (
+  <img className="max-w-[134px] min-w-[134px] rounded-2xl sm:w-1/5 md:w-1/4 lg:w-1/5"
+  src={imgAcme} alt="" />
+);
